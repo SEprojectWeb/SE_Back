cbd720ca0ba056f7d635b955d7ddce65880e1f6d
aa04260 <aa04260@gmail.com>
2024-11-12 18:49:17 +0900

    스프링 기본 설정

---
+++ CompareElec/src/main/java/com/CompareElec/CompareElec/CompareElecApplication.java
@@ -0,0 +1,13 @@
+package com.CompareElec.CompareElec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompareElecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompareElecApplication.class, args);
+	}
+
+}
---
+++ CompareElec/src/test/java/com/CompareElec/CompareElec/CompareElecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CompareElec.CompareElec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompareElecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
